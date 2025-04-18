8d5757796ad8bbd9055d6b526be7a07f6d616701
JoshuaW13 <josh.walters@shaw.ca>
2025-04-18 15:56:12 -0600

    Improved diagonal dash logic and added helpers

--- entities/Warren/States/moveState.gd
+++ entities/Warren/States/moveState.gd
@@ -1,54 +1,67 @@
 extends State
 
 @export var idle_state: State
-@export var dash_state : State
-
-@onready var diagonal_move_timer :Timer = $DiagonalMoveTimer
+@export var dash_state: State
+@onready var diagonal_move_timer: int = 0
 
 var moving_diagonal: bool = false
 
-func setup_timer() ->void:
-	diagonal_move_timer.one_shot = true
-	diagonal_move_timer.wait_time = 0.02
+func get_input_vector() -> Vector2:
+	var h = Input.get_axis("move_left", "move_right")
+	var v = Input.get_axis("move_up", "move_down")
+	return Vector2(h, v)
 
-func _ready() -> void:
-	setup_timer()
+func is_idle_input(input_vector: Vector2) -> bool:
+	return input_vector == Vector2.ZERO
+
+func is_dash_pressed() -> bool:
+	return Input.is_action_just_pressed("dash")
+
+func calculate_velocity(input_vector: Vector2) -> Vector2:
+	if input_vector.length() == 0:
+		return Vector2.ZERO
+	return input_vector.normalized() * move_speed
+
+func handle_diagonal_tracking(input_vector: Vector2) -> void:
+	var is_diagonal = abs(input_vector.x) > 0 and abs(input_vector.y) > 0
+	var is_partial_release = moving_diagonal and (input_vector.x == 0 or input_vector.y == 0)
+
+	if is_diagonal:
+		diagonal_move_timer = 0
+		moving_diagonal = true
+	elif is_partial_release:
+		diagonal_move_timer += 1
+
+	if diagonal_move_timer >2:
+		moving_diagonal = false
+
+func update_last_velocity(input_vector: Vector2, velocity: Vector2) -> void:
+	var is_diagonal = abs(input_vector.x) > 0 and abs(input_vector.y) > 0
+	if !moving_diagonal or is_diagonal:
+		parent.last_velocity = velocity
+
+func apply_movement(velocity: Vector2) -> void:
+	parent.velocity = velocity
+	parent.check_facing()
+	parent.move_and_slide()
 
 func enter() -> void:
 	animation_player.play("Walk")
 
 func process_physics(delta: float) -> State:
-	var horizontal_movement = Input.get_axis("move_left", "move_right")
-	var vertical_movement = Input.get_axis("move_up", "move_down")
-	
-	if horizontal_movement !=0 and vertical_movement !=0:
-		diagonal_move_timer.stop()
-		moving_diagonal = true
+	var input_vector = get_input_vector()
 
-	if horizontal_movement == 0 and vertical_movement == 0:
+	handle_diagonal_tracking(input_vector)
+
+	if is_idle_input(input_vector):
 		return idle_state
-	elif Input.is_action_just_pressed("dash"):
+	if is_dash_pressed():
 		return dash_state
 
-	var input_vector = Vector2(horizontal_movement, vertical_movement)
-	var velocity = Vector2.ZERO
-	
-	if moving_diagonal and horizontal_movement ==0 or vertical_movement ==0:
-		diagonal_move_timer.start()
+	var velocity = calculate_velocity(input_vector)
 
-	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
-		velocity = input_vector * move_speed
-	if !moving_diagonal:
-		parent.last_velocity = velocity
-	elif moving_diagonal and horizontal_movement != 0 and vertical_movement !=0:
-		parent.last_velocity = velocity
+	update_last_velocity(input_vector, velocity)
 
-	parent.velocity = velocity
-	parent.check_facing()
-	parent.move_and_slide()
+	apply_movement(velocity)
+
 	return null
-
-
-func _on_diagonal_move_timer_timeout() -> void:
-	moving_diagonal = false;
